157e15985740b008e63a02e413cffab205b66821
Madhav E <remnukamadhav@gmail.com>
2023-10-13 15:25:44 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Details from './components/information';
+function App() {
+  return (
+    <div className="App">
+      <Details/>
+    </div>
+  );
+}
+
+export default App;
